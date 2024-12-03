5310ebb918e9980a44ebdd61410b2ac0b7bc88d8
K_K_H <gihunkhd5@gmail.com>
2024-12-03 14:43:04 +0900

    데이터전송

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState, useMemo } from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
+
 import Calc1 from './comp/calc/study01'
+
+import Inp1 from './comp/inp/input01'
+import Out1 from './comp/inp/output01'
 
 
 function App() {
@@ -14,6 +19,10 @@ function App() {
           <Route path={"/"} element={<Home />} />
           <Route path={"/about"} element={<About />} />
           <Route path={"/cal1"} element={<Calc1 />} />
+
+          <Route path={"/inp1"} element={<Inp1 />} />
+          <Route path={"/oup1"} element={<Out1 />} />
+
         </Routes>
       </BrowserRouter>
     </div>
@@ -26,6 +35,12 @@ function Home(){
       <Link to="/about">about으로 이동</Link>
       <br></br>
       <Link to="/cal1">cal1으로 이동</Link>
+      <h1>데이터 옮기기</h1>
+
+      <Link to="/inp1">데이터 입력</Link>
+      <br></br>
+      <Link to="/oup1">데이터 출력</Link>
+
     </div>
   );
 }
@@ -34,7 +49,7 @@ function About(){
   return(
     <div style={
       {
-        border : '2px black solid '
+        border : '2px black solid'
       }
     }>
       <Link to="/">Home으로 이동</Link>
