a6c92e20fae22d3f1f1f08dfa1a5ca3ae63e2251
K_K_H <gihunkhd5@gmail.com>
2024-12-03 13:42:23 +0900

    외부 comp불러오기

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState, useMemo } from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Calc1 from './comp/calc/study01'
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <About />
         <Routes>
           <Route path={"/"} element={<Home />} />
           <Route path={"/about"} element={<About />} />
+          <Route path={"/cal1"} element={<Calc1 />} />
         </Routes>
       </BrowserRouter>
     </div>
@@ -21,14 +24,19 @@ function Home(){
     <div>
       <h1>Home</h1>
       <Link to="/about">about으로 이동</Link>
+      <br></br>
+      <Link to="/cal1">cal1으로 이동</Link>
     </div>
   );
 }
 
 function About(){
   return(
-    <div>
-      <h1>About 입니다</h1>
+    <div style={
+      {
+        border : '2px black solid '
+      }
+    }>
       <Link to="/">Home으로 이동</Link>
     </div>
   );
