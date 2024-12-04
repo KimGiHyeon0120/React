fe54b67a045af1c675f8554fea240fe5916a0534
K_K_H <gihunkhd5@gmail.com>
2024-12-04 09:33:36 +0900

    axios 연습

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ import Log1 from './comp/login/login01'
 import Myp1 from './comp/login/mypage01'
 
 
+import Ax1 from './comp/ax/ax01'
+
+
 
 function App() {
   return (
@@ -31,6 +34,10 @@ function App() {
 
           <Route path={"/log1"} element={<Log1 />} />
           <Route path={"/myp1"} element={<Myp1 />} />
+
+
+
+          <Route path={"/ax1"} element={<Ax1 />} />
 
         </Routes>
       </BrowserRouter>
@@ -53,6 +60,9 @@ function Home(){
 
       <h1>회원가입</h1>
       <Link to="/log1">로그인</Link>
+
+      <h1>Axios</h1>
+      <Link to="/ax1">Axios</Link>
 
     </div>
   );
---
+++ src/comp/ax/ax01.js
@@ -0,0 +1,11 @@
+
+
+
+
+export default function Ax1(){
+    return(
+        <div>
+            <h1>axios 연습</h1>
+        </div>
+    )
+}
