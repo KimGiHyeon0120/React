d69e6332dfa63f7f4197eef83456460b5d8c56af
K_K_H <gihunkhd5@gmail.com>
2024-12-11 14:56:40 +0900

    리듀서

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState, useMemo } from 'react';
+import { useEffect, useState, useMemo, useContext } from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 
@@ -23,8 +23,8 @@ import ProItemlist from './comp/login/ItemList'
 
 
 import Red1 from './comp/red/Red01'
-
-
+import Red2 from './comp/red/Red02'
+import ContetxtAction from './comp/red/ContextAction';
 
 
 
@@ -32,56 +32,62 @@ import BoardList1 from './comp/board1/boardlist'
 import BoardWrite1 from './comp/board1/boardwrite'
 import BoardDetali1 from './comp/board1/boarddetail'
 import BoardUpdate1 from './comp/board1/boardupdate'
+import { ContextAPI } from './context/ContextAPi';
 
 function App() {
+  const [parent, setParent] = useState(0);
   return (
     <div className="App">
-      <BrowserRouter>
-      <About />
-        <Routes>
-          <Route path={"/"} element={<Home />} />
-          <Route path={"/about"} element={<About />} />
-          <Route path={"/cal1"} element={<Calc1 />} />
+      <ContextAPI.Provider value={{ parent, setParent }}>
+        <BrowserRouter>
+          <About />
+          <Routes>
 
-          <Route path={"/inp1"} element={<Inp1 />} />
-          <Route path={"/oup1"} element={<Out1 />} />
+            <Route path={"/"} element={<Home />} />
+            <Route path={"/about"} element={<About />} />
+            <Route path={"/cal1"} element={<Calc1 />} />
 
-
-
-          <Route path={"/log1"} element={<Log1 />} />
-          <Route path={"/myp1"} element={<Myp1 />} />
-          <Route path={"/join"} element={<Join1 />} />
-          <Route path={"/login"} element={<Login1 />} />
-          <Route path={"/itemlist"} element={<ProItemlist />} />
+            <Route path={"/inp1"} element={<Inp1 />} />
+            <Route path={"/oup1"} element={<Out1 />} />
 
 
 
-          <Route path={"/ax1"} element={<Ax1 />} />
-
-
-          <Route path={"/ref"} element={<Ref />} />
-
-
-
-          <Route path={"/red01"} element={<Red1 />} />
+            <Route path={"/log1"} element={<Log1 />} />
+            <Route path={"/myp1"} element={<Myp1 />} />
+            <Route path={"/join"} element={<Join1 />} />
+            <Route path={"/login"} element={<Login1 />} />
+            <Route path={"/itemlist"} element={<ProItemlist />} />
 
 
 
+            <Route path={"/ax1"} element={<Ax1 />} />
+
+
+            <Route path={"/ref"} element={<Ref />} />
 
 
 
-          <Route path={"/boardlist1"} element={<BoardList1 />} />
-          <Route path={"/boardwrite1"} element={<BoardWrite1 />} />
-          <Route path={"/boardupdate1"} element={<BoardUpdate1 />} />
-          <Route path="/boarddetail1/:boardId" element={<BoardDetali1 />} />
+            <Route path={"/red01"} element={<Red1 />} />
+            <Route path={"/red02"} element={<Red2 />} />
+            <Route path={'/last'} element={<ContetxtAction />} />
 
-        </Routes>
-      </BrowserRouter>
+
+
+
+
+            <Route path={"/boardlist1"} element={<BoardList1 />} />
+            <Route path={"/boardwrite1"} element={<BoardWrite1 />} />
+            <Route path={"/boardupdate1"} element={<BoardUpdate1 />} />
+            <Route path="/boarddetail1/:boardId" element={<BoardDetali1 />} />
+
+          </Routes>
+        </BrowserRouter>
+      </ContextAPI.Provider>
     </div>
   );
 }
-function Home(){
-  return(
+function Home() {
+  return (
     <div>
       <h1>Home</h1>
       <Link to="/about">about으로 이동</Link>
@@ -121,21 +127,25 @@ function Home(){
       <br></br>
       <Link to="/boardupdate1">BoardUpdate1</Link>
 
-      
+
       <h1>Reducer</h1>
-      <Link to="/red01">red01</Link>
+      <Link to='/red01'>리듀서 01</Link><br />
+      <Link to='/red02'>리듀서 02</Link><br />
+      <Link to='/last'>Context 값 변경하기</Link><br />
     </div>
   );
 }
 
-function About(){
-  return(
-    <div style={
-      {
-        border : '2px black solid'
-      }
-    }>
+function About() {
+
+
+  const { parent, setParent } = useContext(ContextAPI);
+
+  return (
+    <div style={{ border: '2px blue solid' }}>
       <Link to="/">Home으로 이동</Link>
+      <br />
+      context 값: {parent}
     </div>
   );
 }
